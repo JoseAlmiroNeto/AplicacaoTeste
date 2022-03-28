9453d0b2f4ac51208f3976ed18d602119537f1ce
José Almiro <97368866+JoseAlmiroNeto@users.noreply.github.com>
2022-03-28 11:51:26 -0300

    Update index.js

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom"; 
-
 import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
 import HistoryEdu from "@mui/icons-material/HistoryEdu";
-
 import {AreaHeader} from './styled'
-import { Title } from './styled';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function Header(){
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <AreaHeader>
          <div className="container">
              <div className="logo">
-                 <img src="../../../logo.png" />
+                 <Link to="/"><img src="../../../logo.png" /></Link>
 
                  <tile className="title"><strong>Portal de Relacinamento Taugor</strong></tile>
              </div>
@@ -23,7 +33,32 @@ function Header(){
                  <ul>
                      <li><Link to="/"><ReceiptLongIcon/>Solicitações</Link></li>
                      <li><Link to="/nova-solicitacao"><HistoryEdu/>Nova Solicitação</Link></li>
-                     <li><img className="avatar" src="../../../avatar.png"/></li>
+                     
+                     <div className="profile">
+                        <Button
+                            className="profile-btn"
+                            id="basic-button"
+                            aria-controls={open ? 'basic-menu' : undefined}
+                            aria-haspopup="true"
+                            aria-expanded={open ? 'true' : undefined}
+                            onClick={handleClick}
+                        >
+                            <AccountCircleIcon className="profile-btn-icon" />
+                        </Button>
+                        <Menu
+                            id="basic-menu"
+                            anchorEl={anchorEl}
+                            open={open}
+                            onClose={handleClose}
+                            MenuListProps={{
+                            'aria-labelledby': 'basic-button',
+                            }}
+                        >
+                            <MenuItem onClick={handleClose}>Profile</MenuItem>
+                            <MenuItem onClick={handleClose}>My account</MenuItem>
+                            <MenuItem onClick={handleClose}>Logout</MenuItem>
+                        </Menu>
+                    </div>
                  </ul>
              </nav>
          </div>
